d4ac647a808f079e1a65f58e51cfe5f80420dfbe
zimmermannjonas <zimmermannjonas@dv-schulen.de>
2018-06-11 12:42:55 +0200

    11.06.2018

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.ComponentModel;
+
+namespace ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        ICommand bdt1;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void onPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, e);
+            }
+        }
+
+        public ICommand Bdt1
+        {
+            get
+            {
+                return bdt1;
+            }
+
+            set
+            {
+                bdt1 = value;
+            }
+        }
+
+        public MainViewModel()
+        {
+            bdt1 = new UserCommands(SetzeStein);
+        }
+
+        private void SetzeStein(object obj)
+        {
+            onPropertyChanged(new PropertyChangedEventArgs("Background"));
+        }
+    }
+}
---
+++ ViewModel/UserCommands.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    class UserCommands : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action<Object> action;
+        public UserCommands(Action<object> action)
+        {
+            this.action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter != null)
+            {
+                action(parameter);
+            }
+            else
+            {
+                action("Hallo Welt");
+            }
+        }
+    }
+}
