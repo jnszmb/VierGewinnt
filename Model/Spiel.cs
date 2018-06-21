f16e8c07734ea8ff7d58496fc2f41432481edd15
zimmermannjonas <zimmermannjonas@dv-schulen.de>
2018-06-21 15:44:17 +0200

    21.06.2018

--- Model/Spiel.cs
+++ Model/Spiel.cs
@@ -12,6 +12,7 @@ namespace Model
     {
         OleDbConnection con = new OleDbConnection();
         ObservableCollection<Spieler> lstSpieler;
+        String best;
 
         public ObservableCollection<Spieler> LstSpieler
         {
@@ -26,12 +27,25 @@ namespace Model
             }
         }
 
+        public string Best
+        {
+            get
+            {
+                return best;
+            }
+
+            set
+            {
+                best = value;
+            }
+        }
+
         public Spiel()
         {
             con = new OleDbConnection(Properties.SettingsDB.Default.DBCon);
             LstSpieler = new ObservableCollection<Spieler>();
         }
-        public void LoadSpieler()
+        public void LoadSpieler() // Spieler werden aus der Datenbank geladen
         {
             
             OleDbCommand cmd = con.CreateCommand();
@@ -56,5 +70,57 @@ namespace Model
                 con.Open();
             }
         }
+
+        public void BesterSpieler()
+        {
+            for (int i = 1; i < LstSpieler.Count; i++)
+            {
+                if (LstSpieler[i].Punktzahl > LstSpieler[i - 1].Punktzahl)
+                {
+                    Best = "       1." + LstSpieler[i].Name + " : " + LstSpieler[i].Punktzahl;
+                }
+            }
+        }
+        public Spieler Registrieren(String name)
+        {
+            Spieler s = null;
+           
+                s = new Spieler();
+                s.Name = name;
+                s.Punktzahl = 0;
+                this.SpeichernSP(s);
+                LstSpieler.Add(s);
+        
+            return s;
+
+
+        }
+        public void SpeichernSP(Spieler s)
+        {
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.Parameters.AddWithValue("Name", s.Name);
+            cmd.Parameters.AddWithValue("Punkte", s.Punktzahl);
+            cmd.CommandText = "Insert into tSpieler(Spielername,Punktestand)Values(Name,Punkte)";
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "Select @@identity from tSpieler";
+            int id = (int)cmd.ExecuteScalar();
+            s.Id = id;
+        }
+        public void Speichern()
+        {
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.Parameters.Add("Punkte", OleDbType.Integer);
+            cmd.Parameters.Add("Key", OleDbType.Integer);
+            cmd.CommandText = "Update tSpieler set Punktestand = Punkte where SpielerID = Key";
+            foreach(Spieler sp in LstSpieler)
+            {
+                cmd.Parameters["Punkte"].Value = sp.Punktzahl;
+                cmd.Parameters["Key"].Value = sp.Id;
+                cmd.ExecuteNonQuery();
+            }
+            con.Close();
+        }
+
     }
 }
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -7,13 +7,20 @@ using System.Windows.Input;
 using System.ComponentModel;
 using Model;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace ViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
         Spiel spiel;
-        ICommand bdt1;
+        int punkteSp1;
+        int punkteSp2;
+        
+        ICommand btdStart;
+        ICommand btdReg;
+        ICommand btdSave;
+        ICommand btd1;
   
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -26,33 +33,131 @@ namespace ViewModel
             }
         }
 
-        public ICommand Bdt1
-        {
-            get
-            {
-                return bdt1;
-            }
-
-            set
-            {
-                bdt1 = value;
-            }
-        }
-
         public MainViewModel()
         {
             spiel = new Spiel();
-            spiel.LoadSpieler();
-            bdt1 = new UserCommands(SetzeStein);
+            spiel.LoadSpieler(); // Spieler werden aus der Datenbank geladen
+            spiel.BesterSpieler(); // Der beste Spieler wird ermittelt
+            BtdReg = new UserCommands(Registrieren);// ein neuer Spieler wird über den Button-Command aufgenommen
+            BtdStart = new UserCommands(starten);
+            BtdSave = new UserCommands(speichern);
+            Btd1 = new UserCommands(setzeStein);
+
+
         }
+
+        private void setzeStein(object obj)
+        {
+            
+        }
+
+        private void speichern(object obj)
+        {
+            spiel.Speichern();
+        }
+
+        private void Registrieren(object obj) 
+        {
+            String name = (String)obj;
+            Spieler s = spiel.Registrieren(name);
+        }
+
+        private void starten(object obj)
+        {
+            ConsoleColor Background = (ConsoleColor)obj;
+            Background = ConsoleColor.Red;
+            onPropertyChanged(new PropertyChangedEventArgs("Background"));
+        }
+
         public ObservableCollection<Spieler>LSTSpieler
         {
             get { return spiel.LstSpieler; }
         }
+        public String Best
+        {
+            get { return spiel.Best; }
+        }
+
+        public int PunkteSp1
+        {
+            get
+            {
+                return punkteSp1;
+            }
+
+            set
+            {
+                punkteSp1 = value;
+            }
+        }
+
+        public int PunkteSp2
+        {
+            get
+            {
+                return punkteSp2;
+            }
+
+            set
+            {
+                punkteSp2 = value;
+            }
+        }
+
+        public ICommand BtdReg
+        {
+            get
+            {
+                return btdReg;
+            }
+
+            set
+            {
+                btdReg = value;
+            }
+        }
+
+        public ICommand BtdStart
+        {
+            get
+            {
+                return btdStart;
+            }
+
+            set
+            {
+                btdStart = value;
+            }
+        }
+
+        public ICommand BtdSave
+        {
+            get
+            {
+                return btdSave;
+            }
+
+            set
+            {
+                btdSave = value;
+            }
+        }
+
+        public ICommand Btd1
+        {
+            get
+            {
+                return btd1;
+            }
+
+            set
+            {
+                btd1 = value;
+            }
+        }
 
         private void SetzeStein(object obj)
         {
-
             onPropertyChanged(new PropertyChangedEventArgs("Background"));
         }
     }
